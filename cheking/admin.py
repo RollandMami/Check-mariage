6ad2a6402c2954052a21fb6a17c06793845315a9
Rolland359 <rolland@futurmap.com>
2025-09-09 09:53:00 +0300

    api approved

--- cheking/admin.py
+++ cheking/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
 
 class QrAdmin(admin.ModelAdmin):
     list_display=("code", "table")
@@ -11,11 +13,16 @@ class InviteAdmin(admin.ModelAdmin):
     search_fields = ("nom", "invitation__code", "ville__nom", "quartier__nom")
     list_filter = ("est_present", "attente_de_validation", "ville", "quartier")
 
+class CustomAdminSite(admin.AdminSite):
+    site_url = '/cheking/'  # Définit le lien du bouton "View site"
 
 # Register your models here.
-admin.site.register(Ville)
-admin.site.register(Quartier)
-admin.site.register(Invite, InviteAdmin)
-admin.site.register(QrCode, QrAdmin)
-admin.site.register(Tables)
-admin.site.register(LienAmitie)
+custom_admin_site = CustomAdminSite(name='custom_admin')
+custom_admin_site.register(Ville)
+custom_admin_site.register(Quartier)
+custom_admin_site.register(Invite, InviteAdmin)
+custom_admin_site.register(QrCode, QrAdmin)
+custom_admin_site.register(Tables)
+custom_admin_site.register(LienAmitie)
+custom_admin_site.register(User, UserAdmin)    # <-- Ajoute ceci
+custom_admin_site.register(Group, GroupAdmin)
--- mariage/urls.py
+++ mariage/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from cheking.admin import custom_admin_site
 
 urlpatterns = [
     path('cheking/', include('cheking.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', custom_admin_site.urls),
     
 ]
+
